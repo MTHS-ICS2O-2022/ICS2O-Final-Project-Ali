8352bf916e2bc8f93055569a821dbe4f465e4e4e
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-10 14:25:06 -0500

    created several assets, start scene and game.js

---
+++ js/game.js
@@ -0,0 +1,36 @@
+/* global phaser */
+// Copyright (c) 2020 Ali Mugamai All rights reserved
+//
+// Created by: Ali Mugamai
+// Created on: JAN 2023
+// This file contains the JS functions for index.html
+import startScene from "./startScene.js";
+import GameScene from "./gameScene.js";
+
+const startScene = new startScene();
+const gameScene = new GameScene();
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background colour //
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
+
+const game = new Phaser.Game(config);
+
+game.scene.add("menuScene", menuScene);
+game.scene.add("startScene", startScene);
+
+game.scene.start("startScene");
