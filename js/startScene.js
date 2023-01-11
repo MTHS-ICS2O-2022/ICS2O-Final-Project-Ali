bf98afe4f6d9590f821e5fb8d821b88935609c48
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2023-01-11 18:44:46 +0000

    Prettified Code!

--- js/startScene.js
+++ js/startScene.js
@@ -6,45 +6,47 @@
 // This file contains the JS functions for index.html
 
 class StartScene extends Phaser.Scene {
-    constructor() {
-      super({ key: "startScene" });
-  
-      this.startSceneBackgroundImage = null;
-      this.startButton = null;
-    }
-    /**
-     *@param {object} data
-     */
-    Init(data) {
-      this.cameras.main.setBackgroundColor("ffffff");
-    }
-  
-    preload() {
-      console.log("Start Scene");
-      this.load.image("menuSceneBackground", "./assets/river_fighting_startscene.jpg");
-      this.load.image("startButton", "./assets/start_button.png");
-    }
-  
-    create(data) {
-      this.startSceneBackgroundImage = this.add.sprite(
-        0,
-        0,
-        "startSceneBackgroundImage"
-      );
-      this.startSceneBackgroundImage.x = 1920 / 2;
-      this.startSceneBackgroundImage.y = 1080 / 2;
-  
-      this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton");
-      this.startButton.setInteractive({ useHandCursor: true });
-      this.startButton.on("pointerdown", () => this.clickButton());
-    }
-  
-    update(time, delta) {}
-  
-    clickButton() {
-      this.scene.start("gameScene");
-    }
+  constructor() {
+    super({ key: "startScene" });
+
+    this.startSceneBackgroundImage = null;
+    this.startButton = null;
   }
-  
-  export default startScene;
-  
+  /**
+   *@param {object} data
+   */
+  Init(data) {
+    this.cameras.main.setBackgroundColor("ffffff");
+  }
+
+  preload() {
+    console.log("Start Scene");
+    this.load.image(
+      "menuSceneBackground",
+      "./assets/river_fighting_startscene.jpg"
+    );
+    this.load.image("startButton", "./assets/start_button.png");
+  }
+
+  create(data) {
+    this.startSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "startSceneBackgroundImage"
+    );
+    this.startSceneBackgroundImage.x = 1920 / 2;
+    this.startSceneBackgroundImage.y = 1080 / 2;
+
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton");
+    this.startButton.setInteractive({ useHandCursor: true });
+    this.startButton.on("pointerdown", () => this.clickButton());
+  }
+
+  update(time, delta) {}
+
+  clickButton() {
+    this.scene.start("gameScene");
+  }
+}
+
+export default startScene;
