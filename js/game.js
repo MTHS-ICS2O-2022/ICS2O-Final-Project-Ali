beb593ea40c6bc1bba3473b54bccd7e061e24eee
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-12 14:42:40 -0500

    menu scene

--- js/game.js
+++ js/game.js
@@ -4,15 +4,9 @@
 // Created by: Ali Mugamai
 // Created on: NOV 2022
 // This file contains the JS functions for index.html
-import SplashScene from "./splashScene.js";
-import TitleScene from "./titleScene.js";
 import MenuScene from "./menuScene.js";
-import GameScene from "./gameScene.js";
 
-const splashScene = new SplashScene();
-const titleScene = new TitleScene();
 const menuScene = new MenuScene();
-const gameScene = new GameScene();
 
 const config = {
   type: Phaser.AUTO,
@@ -34,9 +28,6 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-game.scene.add("splashScene", splashScene);
-game.scene.add("titleScene", titleScene);
 game.scene.add("menuScene", menuScene);
-game.scene.add("gameScene", gameScene);
 
-game.scene.start("splashScene");
+game.scene.start("menuScene");
