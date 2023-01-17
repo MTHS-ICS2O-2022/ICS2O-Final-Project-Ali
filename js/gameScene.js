956a42f2024d10323b94c2d66bd5792b6c1eba69
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-17 13:48:18 -0500

    sprites

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,8 +10,7 @@ class GameScene extends Phaser.Scene {
     super({ key: "gameScene" });
 
     this.background = null;
-    this.p1 = null;
-    this.p2 = null
+    this.ship = null;
     this.fireMissle = false;
     this.score = 0;
     this.scoreText = null;
@@ -34,20 +33,20 @@ class GameScene extends Phaser.Scene {
   preload() {
     console.log("Game Scene");
 
-        this.load.image("riverBackground", "./assets/river_fighting_scene.jpg");
-        this.load.image("ship", "./assets/spaceShip.png");
-        this.load.image("missile", "./assets/blue_laser.png");
-      // sound
-        this.load.audio("laser", "assets/laser1.wav");
-        this.load.image("alien", "assets/alien.png");
-        this.load.audio("laser", "./assets/laser1.wav");
-        this.load.audio("explosion", "./assets/barrelExploding.wav");
-        this.load.audio("bomb", "./assets/bomb.wav");
-    }
+    this.load.image("starBackground", "./assets/river_fighting_scene.jpg");
+    this.load.image("ship", "./assets/player_1.png");
+    this.load.image("missile", "./assets/blue_laser.png");
+    // sound
+    this.load.audio("laser", "assets/laser1.wav");
+    this.load.image("alien", "assets/alien.png");
+    this.load.audio("laser", "./assets/laser1.wav");
+    this.load.audio("explosion", "./assets/barrelExploding.wav");
+    this.load.audio("bomb", "./assets/bomb.wav");
+  }
 
-    create(data) {
-        this.background = this.add.image(0, 0, "riverBackground").setScale(2.0);
-        this.background.setOrigin(0, 0);
+  create(data) {
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
     this.scoreText = this.add.text(
       10,
@@ -56,7 +55,7 @@ class GameScene extends Phaser.Scene {
       this.scoreTextStyle
     );
 
-        this.player1 = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
     this.missileGroup = this.physics.add.group();
 
@@ -117,53 +116,30 @@ class GameScene extends Phaser.Scene {
       }
     }
 
-    update(time, delta) {
-        const keyLeftObj = this.input.keyboard.addKey("LEFT");
-        const keyRightObj = this.input.keyboard.addKey("RIGHT");
-        const keyDownObj = this.input.keyboard.addKey("DOWN");
-
-        const keySpaceObj = this.input.keyboard.addKey("SPACE");
-        const keySpaceObj = this.input.keyboard.addKey("SPACE");
-        const keySpaceObj = this.input.keyboard.addKey("SPACE");
-
-        if (keyLeftObj.isDown === true) {
-        this.player1.x -= 15;
-        if (this.player1.x < 0) {
-            this.player1.x = 0;
-        }
-        }
-        if (keyRightObj.isDown === true) {
-        this.ship.x += 15;
-        if (this.player1.x > 1920) {
-            this.player1.x = 1920;
-        }
+    if (keySpaceObj.isDown === true) {
+      if (this.firemissile === false) {
+        this.firemissile = true;
+        const aNewMissile = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          "missile"
+        );
+        this.missileGroup.add(aNewMissile);
+        this.sound.play("laser");
+      }
     }
 
-        if (keyDownObj.isDown === true) {
-        if (this.firemissile === false) {
-            this.firemissile = true;
-            const aNewMissile = this.physics.add.sprite(
-            this.player1.x,
-            this.player1.y,
-            "missile"
-            );
-            this.missileGroup.add(aNewMissile);
-            this.sound.play("laser");
-        }
-        }
-
-        if (keyDownObj.isUp === true) {
-        this.firemissile = false;
-        }
+    if (keySpaceObj.isUp === true) {
+      this.firemissile = false;
+    }
 
     this.missileGroup.children.each(function (item) {
-        item.y = item.y - 15;
-        if (item.y < 0) {
+      item.y = item.y - 15;
+      if (item.y < 0) {
         item.destroy();
-            }
-        });
-    }
-}
+      }
+    });
+  }
 }
 
 export default GameScene;
