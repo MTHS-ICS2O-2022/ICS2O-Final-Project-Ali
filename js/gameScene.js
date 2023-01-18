70c01e1015326d27692140dd5df3c4936a6a82d4
Ali-Mugamai <Ali-Mugamai@users.noreply.github.com>
2023-01-18 19:42:06 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -61,7 +61,7 @@ class GameScene extends Phaser.Scene {
 
     this.blue_laserGroup = this.physics.add.group();
     this.red_laserGroup = this.physics.add.group();
-    blue_laserGroup.scale.x = (256, 256)
+    blue_laserGroup.scale.x = (256, 256);
     this.physics.add.collider(
       this.red_laserGroup,
       this.player1,
